6b428cb468e81fffd709c2253b80db55abc44f84
Ido Frenkel <ido.frenkel@mobileye.com>
2022-11-28 16:28:28 +0200

    add GracefulExit

--- serial_sniffer.py
+++ serial_sniffer.py
@@ -6,6 +6,7 @@ import logging
 import os
 import pathlib
 import re
+import signal
 import sys
 from datetime import datetime
 
@@ -14,6 +15,14 @@ from serial import PosixPollSerial
 logger = logging.getLogger(__name__)
 
 ANSI_ESCAPE = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])|[\x00-\x09\x0B-\x1F]|[\x7f-\xA0]")
+
+
+class GracefulExit(SystemExit):
+    pass
+
+
+def signal_handler(signum, frame):
+    raise GracefulExit(1)
 
 
 def delete_ansi_esc(text: bytes | str):
@@ -36,8 +45,58 @@ def lock_dev(port: pathlib.Path):
             os.unlink(f"/var/lock/LCK..{link}")
 
 
+def sniff_port(
+    port: pathlib.Path | str,
+    output: pathlib.Path | str | None = None,
+    delete_esc_chars: bool = True,
+    add_timestamp: bool = True,
+) -> int:
+    port, output = pathlib.Path(port), pathlib.Path(output)
+    with lock_dev(port):
+        return _sniff(port, output, delete_esc_chars, add_timestamp)
+
+
+def _sniff(
+    port: pathlib.Path,
+    output: pathlib.Path | None = None,
+    delete_esc_chars: bool = True,
+    add_timestamp: bool = True,
+) -> int:
+    signal.signal(signal.SIGTERM, signal_handler)
+    logger.info(f"Start sniffing port: {port}")
+    serial = PosixPollSerial(
+        port=str(port),
+        baudrate=115200,
+        timeout=30,
+    )
+    if output is not None:
+        os.makedirs(output.parent, exist_ok=True)
+        output = open(output, "w")
+    else:
+        output = sys.stdout
+    with output as output_file:
+        try:
+            read_lines(delete_esc_chars, serial, output_file, add_timestamp)
+        except (KeyboardInterrupt, GracefulExit):
+            logger.info(f"Done sniffing port: {port}")
+            return 0
+
+
+def read_lines(delete_esc_chars, serial, output_file, add_timestamp):
+    while True:
+        line = serial.readline()
+        if delete_esc_chars:
+            line = delete_ansi_esc(line)
+        else:
+            line = line.decode()
+        if add_timestamp:
+            line = f"{str(datetime.now().strftime('%Y-%m-%d %H:%M:%S,%f'))} {line}"
+        output_file.write(line)
+        output_file.flush()
+
+
 def main() -> int:
-    logging.basicConfig()
+    logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser(prog="serial-sniffer")
     parser.add_argument(
         "-p",
@@ -67,47 +126,6 @@ def main() -> int:
     )
     args = parser.parse_args()
     sniff_port(args.port, args.output, args.delete_esc_chars, args.add_timestamp)
-
-
-def sniff_port(
-    port: pathlib.Path,
-    output: pathlib.Path | None = None,
-    delete_esc_chars: bool = True,
-    add_timestamp: bool = True,
-):
-    with lock_dev(port):
-        _sniff(port, output, delete_esc_chars, add_timestamp)
-
-
-def _sniff(port, output, delete_esc_chars, add_timestamp):
-    logger.info(f"Start sniffing port: {port}")
-    serial = PosixPollSerial(
-        port=str(port),
-        baudrate=115200,
-        timeout=30,
-    )
-    if output is not None:
-        output = open(output, "w")
-    else:
-        output = sys.stdout
-    with output as output_file:
-        try:
-            read_lines(delete_esc_chars, serial, output_file, add_timestamp)
-        except KeyboardInterrupt:
-            logger.info(f"Done sniffing port: {port}")
-
-
-def read_lines(delete_esc_chars, serial, output_file, add_timestamp):
-    while True:
-        line = serial.readline()
-        if delete_esc_chars:
-            line = delete_ansi_esc(line)
-        else:
-            line = line.decode()
-        if add_timestamp:
-            line = f"{str(datetime.now().strftime('%Y-%m-%d %H:%M:%S,%f'))} {line}"
-        output_file.write(line)
-        output_file.flush()
 
 
 if __name__ == "__main__":
